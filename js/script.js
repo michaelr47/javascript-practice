e8770c43604478246f20b663baec563e8fd592f5
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-09-13 12:10:18 -0700

    working with .length and bigints and typeof

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ multi line comment */
 
 console.log("Michael");
 console.log("Chinese food");
+
+let longWord = "Supercalifragilisticexpialidocious";
+
+console.log(longWord.length);
+
+console.log(typeof 23);
+
+
+const bigInt = 54589474987658574564385884368n;
+console.log(bigInt);
