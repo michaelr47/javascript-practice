6a0eeb6b8b055df9759fbf9ff0e4d9abb252ebe8
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-09-26 18:56:51 -0700

    refactored code, utilizied window onload to call func

--- js/script.js
+++ js/script.js
@@ -1,80 +1,34 @@
-// define a variable name userName and set it to an empty string
-function eightBallGame() {
 
+
+function eightBallGame() {
 let userName = prompt('Enter name');
-// create a ternary expression if the user enters a name — like 'Jane' — use string interpolation to log 'Hello,
-// Jane!' to the console. Otherwise, simply log Hello!.
-userName === true ? document.getElementById('demo').innerHTML = 'hello' + userName + '!' : document.getElementById('demo').innerHTML = 'hello stranger!';
-// document.getElementById('demo').innerHTML = 'hello' + userName + '!'
-// create a variable name userQuestion and input a question
+
+document.getElementById('demo').innerHTML = `Hello ${userName}!`;
+
 let userQuestion = prompt('Ask question here...');
-// write a console log stating what you asked, you can log user name if you wish
-console.log(userQuestion);
-// create another variable 'randomNumber' = to Math.floor(Math.random() * 10);
 let randomNumber = Math.floor(Math.random() * 8);
-// generate a num through 0-10
-//create one more var 'eightBall' and equal it to an empty string
-let eightBall = '';
-// create an if statement utilizing these strings..
+
 switch (randomNumber) {
     case 0:
-        console.log('It is certain');
-        break;
+        return 'It is certain';
     case 1:
-        console.log('It is decidedly so');
-        break;
+        return 'It is decidedly so';
     case 2:
-        console.log('Reply hazy try again');
-        break;
+        return 'Reply hazy try again';
     case 3:
-        console.log('Cannot predict now');
-        break;
+        return 'Cannot predict now';
     case 4: 
-        console.log('Do not count on it');
-        break;
+        return 'Do not count on it';
     case 5: 
-        console.log('My sources say no');
-        break;
+        return 'My sources say no';
     case 6: 
-        console.log('Outlook not so good');
-        break;
+        return 'Outlook not so good';
     case 7: 
-        console.log('Signs point to yes');
-        break;
-    case 8: 
-        console.log('no');
-        break;
-    case 9: 
-        console.log('yes');
-        break;
-    case 10: 
-        console.log('definitely YES');
-        break;
+        return 'Signs point to yes';
+    
+    }   
 }
 
-// 'It is certain'
-// 'It is decidedly so'
-// 'Reply hazy try again'
-// 'Cannot predict now'
-// 'Do not count on it'
-// 'My sources say no'
-// 'Outlook not so good'
-// 'Signs point to yes'
-console.log(eightBall);
-
+window.onload = function() {
+    document.getElementById('answer').innerHTML = eightBallGame();
 }
-
-// document.getElementById('demo').innerHTML = randomNumber;
-
-// if randomNumber is 0, then save an anwser to eightBall, and so on
-// write a console log to print the eightBall value
-//console.log(randomNumber);
-
-//  function name() {
-//     document.getElementById('demo').innerHTML = eightBallGame();
-// }
-
-// window.onload = function() {
-//    let myBtn = document.getElementById('btn');
-//     myBtn.onclick = name();
-// }
